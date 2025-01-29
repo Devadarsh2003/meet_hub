1376ef6b19e1476c113694991f0a69ce761a0704
Devadarsh2003 <achudevadarh@gmail.com>
2025-01-29 22:59:02 +0530

    few changes made to the initial layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,17 @@
+import { Button } from "@/components/ui/button";
+import { SignedOut, SignInButton, UserButton, SignedIn } from "@clerk/nextjs";
 import Image from "next/image";
+
 
 export default function Home() {
   return (
-    <div >
-      <h1>Home</h1>
+    <div className='m-10'>
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
+        <SignedIn>
+        <UserButton />
+      </SignedIn>
     </div>
   );
 }
